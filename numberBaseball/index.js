7afa083a4e3234d7b0855b6397fc9c4ebe1db423
lhk3337 <lhk3337@naver.com>
2021-03-31 15:29:20 +0900

    random number

--- numberBaseball/index.js
+++ numberBaseball/index.js
@@ -2,6 +2,13 @@ const input = document.querySelector("#input");
 const check = document.querySelector("#check");
 const result = document.querySelector("#result");
 
+let answer;
+
+const first = Math.floor(Math.random()) * 10;
+const second = Math.floor(Math.random()) * 10;
+const third = Math.floor(Math.random()) * 10;
+const fourth = Math.floor(Math.random()) * 10;
+
 let count = 0;
 check.addEventListener("click", () => {
   const value = input.value; //문자열 ex) '3046' true truthy
@@ -9,4 +16,3 @@ check.addEventListener("click", () => {
   if (value && value.length === 4) {
   }
 });
-let answer;
