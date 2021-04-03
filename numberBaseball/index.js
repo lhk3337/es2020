16aecfb814205acc57b7d9766b81581352ddbfc6
lhk3337 <lhk3337@naver.com>
2021-04-03 15:18:12 +0900

    while문을 for문으로, 배열 비교 확인

--- numberBaseball/index.js
+++ numberBaseball/index.js
@@ -9,11 +9,19 @@ while (n <= 3) {
   //splice때문에 배열인덱스가 하나씩 줄어들기 때문에, index도 하나씩 줄어들어 줘야 하기 때문에 n만큼 빼줌.
   //numbers=[0, 1, 2, 3], numbers[index], 이때 index=5이면 undefined
   answer.push(numbers[index]); //배열의 끝에 하나 이상의 요소를 추가, numbers[3]의 값이 answer의 마지막 인덱스에 저장
-  numbers.splice(index, 1); //랜덤으로 발생한 index 값 배열부터 한개를 제거,
+  numbers.splice(index, 1); //랜덤으로 발생한 index 값 배열부터 하나를 제거,
   //만일 numbers.splice(4, 1)이면 numbers=[0, 1, 2, 3, 5, 6 ,7 8, 9] 4번째 인덱스의 값 4 삭제 후 인덱스 5번째 있던 값이 4번째 인덱스로 이동한다.
   n = n + 1;
 }
 console.log(answer);
+
+//for문
+// for (let n = 0; n <= 3; n += 1) {
+//   const index = Math.floor(Math.random() * (10 - n));
+//   answer.push(numbers[index]);
+//   numbers.splice(index, 1);
+// }
+
 // let answer = [
 //   String(Math.floor(Math.random()) * 10),
 //   String(Math.floor(Math.random()) * 10),
@@ -34,6 +42,9 @@ let count = 0;
 check.addEventListener("click", () => {
   const value = input.value; //문자열 ex) '3046' true truthy
   // 0, '', NaN, false, null, undefined -> false value
-  if (value && value.length === 4) {
+  if (answer.join("") === value) {
+    result.appendChild(document.createTextNode("HR"));
+  } else {
+    console.log("다르다");
   }
 });
