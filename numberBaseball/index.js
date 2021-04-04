c3f1ff61c3b58a7ebd47452a2c4b8f1f945d20c9
lhk3337 <lhk3337@naver.com>
2021-04-04 21:59:32 +0900

    append와 appendChild비교 및 버그 수정

--- numberBaseball/index.js
+++ numberBaseball/index.js
@@ -1,19 +1,31 @@
 const input = document.querySelector("#input");
 const check = document.querySelector("#check");
 const result = document.querySelector("#result");
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; // 1. 일일이 숫자 나열
+
+// let numbers = [];
+// for (let n = 0; n <= 9; n++) {
+//   numbers.push(n);
+// }// 2. for문 이용
+
+let numbers = Array(10)
+  .fill()
+  .map((v, i) => i);
+// 3. map 이용
+
 let answer = [];
 let n = 0;
 while (n <= 3) {
-  const index = Math.floor(Math.random() * (10 - n)); // numers.splice를 하면 배열이 하나씩 줄어든다.
+  const index = Math.floor(Math.random() * numbers.length); // numers.splice를 하면 배열이 하나씩 줄어든다.
   //splice때문에 배열인덱스가 하나씩 줄어들기 때문에, index도 하나씩 줄어들어 줘야 하기 때문에 n만큼 빼줌.
   //numbers=[0, 1, 2, 3], numbers[index], 이때 index=5이면 undefined
   answer.push(numbers[index]); //배열의 끝에 하나 이상의 요소를 추가, numbers[3]의 값이 answer의 마지막 인덱스에 저장
   numbers.splice(index, 1); //랜덤으로 발생한 index 값 배열부터 하나를 제거,
-  //만일 numbers.splice(4, 1)이면 numbers=[0, 1, 2, 3, 5, 6 ,7 8, 9] 4번째 인덱스의 값 4 삭제 후 인덱스 5번째 있던 값이 4번째 인덱스로 이동한다.
+  //만일 numbers.splice(4, 1)이면 numbers=[0, 1, 2, 3, 5, 6, 7, 8, 9] 4번째 인덱스의 값 4 삭제 후 인덱스 5번째 있던 값이 4번째 인덱스로 이동한다.
   n = n + 1;
 }
-console.log(answer);
+console.log(answer); //답
 
 //for문
 // for (let n = 0; n <= 3; n += 1) {
@@ -65,6 +77,7 @@ check.addEventListener("click", () => {
       `${input.value}: ${strike} strike ${ball} ball`,
       document.createElement("br")
     );
+    //result.appendChild(document.createTextNode(`${input.value}: ${strike} strike ${ball} ball`)) ,appendChild를 사용할 경우
     if (count > 10) {
       result.appendChild(
         document.createTextNode(`Game Over : ${answer.join("")}`)
